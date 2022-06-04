223509776e89531ff84ab7f1f5dac7ff22d772ef
neginkheirmand <neginkheirmand@gmail.com>
2022-06-04 11:41:40 +0430

    updated code

--- client.py
+++ client.py
@@ -7,7 +7,8 @@ PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
+    a = input("client: ")
+    s.sendall(bytes(bytearray(a, encoding='utf-8')))
     data = s.recv(1024)
 
 print(f"Received {data!r}")
--- server.py
+++ server.py
@@ -15,4 +15,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
+            print("server receive: "  + data.decode() + "\nand responded with: " + data.decode() )
             conn.sendall(data)
