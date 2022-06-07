d76fa4b44376cd70d9d8cc0a4925ec30ecea5675
neginkheirmand <neginkheirmand@gmail.com>
2022-06-07 21:40:45 +0430

    done with the code

--- client.py
+++ client.py
@@ -4,8 +4,8 @@ from codecs import namereplace_errors
 import socket
 import json
 import psutil
-  
-
+import time
+import sys
 
 def getCpuPercentage(time):
     # print('The CPU usage is: ', psutil.cpu_percent(4))
@@ -24,14 +24,50 @@ def getData():
 
 
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    # a = input("client: ")
-    a = "cpu spec "+ str(getData())
-    s.sendall(bytes(bytearray(a, encoding='utf-8')))
-    data = s.recv(1024)
+def end():
+    global s
+    try:
+        s.shutdown(socket.SHUT_RDWR)
+        s.close()
+    except:
+        return
 
-print(f"Received {data!r}")
+def create_agent():
+    HOST = "127.0.0.1"  # The server's hostname or IP address
+    PORT = 2004  # The port used by the server
+    try:
+        global s
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            port_number = s.recv(1024).decode()
+            print("agent on port", port_number, "started, connection to the server stablished")
+            time.sleep(1)
+            while(True):
+                a = str(getData())
+                time.sleep(1)
+                s.sendall(bytes(bytearray(a, encoding='utf-8')))
+                data = s.recv(1024)
+                if not data:
+                    end()
+                    # exit()
+                    return
+    except KeyboardInterrupt:
+        print("keyboard interrupt detected, exiting...")
+        end()
+        sys.exit(0)
+    except:
+        end()
+        return
+
+
+if __name__ == '__main__':
+    try:
+        while(True):
+            create_agent()
+            end()
+    except KeyboardInterrupt:
+        print("keyboard interrupt detected, exiting...")
+        end()
+        sys.exit(0)
